6a1aeff275ddfb68561a73ba620d6a154f63fa3c
krishanthisera <krishees@gmail.com>
2020-11-22 09:55:16 +1030

    Channel added issues to fix

--- go-routines/main.go
+++ go-routines/main.go
@@ -11,31 +11,67 @@ var cache = map[int]Book{}
 var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func main() {
+	//Creating a waight group
+	//Waitgroup will keep the process alive until the execution of the subrotine finished
 	wg := &sync.WaitGroup{}
+	//Mutexes allow a paticular operation to do changes on a resource
 	mx := &sync.RWMutex{}
+
+	//Chennels for cache and database
+	dbCh := make(chan Book)
+	cacheCh := make(chan Book)
 	for i := 0; i < 10; i++ {
-		wg.Add(2)
+
+		wg.Add(2) //Define Number of wait groups
 		id := rnd.Intn(10) + 1
-		go func(id int, wg *sync.WaitGroup, mx *sync.RWMutex) {
+
+		/*************************************************************************
+		*Routine 01
+		*************************************************************************/
+		go func(id int, wg *sync.WaitGroup, mx *sync.RWMutex, ch chan<- Book) {
 			if b, ok := querryCache(id, mx); ok {
-				fmt.Println("From the cache")
-				fmt.Println(b)
+				ch <- b
 			}
 			wg.Done()
-		}(id, wg, mx)
-		go func(id int, wg *sync.WaitGroup, mx *sync.RWMutex) {
+		}(id, wg, mx, cacheCh)
+
+		/*************************************************************************
+		*Routine 02
+		*************************************************************************/
+		go func(id int, wg *sync.WaitGroup, mx *sync.RWMutex, ch chan<- Book) {
 			if b, ok := querryDtabase(id, mx); ok {
-				fmt.Println("From the database")
-				fmt.Println(b)
+				mx.Lock()
+				cache[id] = b
+				mx.Unlock()
+				ch <- b
 			}
 			wg.Done()
-		}(id, wg, mx)
-		//fmt.Println("Book not found")
-		//time.Sleep(150 * time.Microsecond)
+		}(id, wg, mx, dbCh)
+
+		/*************************************************************************
+		*Routine 03
+		*************************************************************************/
+		go func(cacheCh, dbCh chan Book) {
+			select {
+			case b := <-cacheCh:
+				fmt.Println("Value from cache!!")
+				fmt.Println(b)
+				<-dbCh
+			case b := <-dbCh:
+				fmt.Println("Value from database!!")
+				fmt.Println(b)
+			}
+		}(cacheCh, dbCh)
+
 	}
+
 	//println(id)
 	wg.Wait()
+	close(dbCh)
+	close(cacheCh)
+
 }
+
 func querryCache(id int, mx *sync.RWMutex) (Book, bool) {
 	mx.RLock()
 	b, ok := cache[id]
