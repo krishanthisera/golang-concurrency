e2049705fdd5907d6083ac22a0c6c4f80638372c
krishanthisera <krishees@gmail.com>
2020-11-22 08:58:53 +1030

    Channels with for loops

--- go-channels/sample-channel/main.go
+++ go-channels/sample-channel/main.go
@@ -9,15 +9,17 @@ func main() {
 	ch := make(chan int, 5)
 	wg := &sync.WaitGroup{}
 	wg.Add(2)
-	go func(w *sync.WaitGroup, c chan<- int) {
-		//time.Sleep(5 * time.Second)
-		//fmt.Println(<-ch)
-		c <- 22
-		//fmt.Println(<-ch)
+	go func(w *sync.WaitGroup, c <-chan int) {
+		for msg := range ch {
+			fmt.Println(msg)
+		}
 		wg.Done()
 	}(wg, ch)
-	go func(w *sync.WaitGroup, c <-chan int) {
-		fmt.Println(<-ch)
+	go func(w *sync.WaitGroup, c chan<- int) {
+		for i := 0; i < 10; i++ {
+			ch <- i
+		}
+		close(ch)
 		wg.Done()
 	}(wg, ch)
 	wg.Wait()
