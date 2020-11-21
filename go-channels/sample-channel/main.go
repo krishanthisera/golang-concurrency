2281429983c6829871fc2f26e7c031808be08ece
krishanthisera <krishees@gmail.com>
2020-11-21 22:12:12 +1030

    Channle-direction

--- go-channels/sample-channel/main.go
+++ go-channels/sample-channel/main.go
@@ -9,16 +9,15 @@ func main() {
 	ch := make(chan int, 5)
 	wg := &sync.WaitGroup{}
 	wg.Add(2)
-	go func(w *sync.WaitGroup, c chan int) {
+	go func(w *sync.WaitGroup, c chan<- int) {
 		//time.Sleep(5 * time.Second)
-		fmt.Println(<-ch)
-		fmt.Println(<-ch)
-		fmt.Println(<-ch)
+		//fmt.Println(<-ch)
+		c <- 22
+		//fmt.Println(<-ch)
 		wg.Done()
 	}(wg, ch)
-	go func(w *sync.WaitGroup, c chan int) {
-		c <- 91
-		c <- 22
+	go func(w *sync.WaitGroup, c <-chan int) {
+		fmt.Println(<-ch)
 		wg.Done()
 	}(wg, ch)
 	wg.Wait()
